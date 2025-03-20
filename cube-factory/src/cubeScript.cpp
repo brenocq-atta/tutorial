fd2793cc46c81164f59263496ed3aea5fa8ad49d
Breno Cunha Queiroz <breno1423@gmail.com>
2025-03-20 20:47:18 +0100

    Chore: Update cube-factory to atta 0.3.6

--- cube-factory/src/cubeScript.cpp
+++ cube-factory/src/cubeScript.cpp
@@ -11,53 +11,52 @@
 #include <atta/resource/interface.h>
 #include <atta/resource/resources/material.h>
 
-namespace scr = atta::script;
-namespace cmp = atta::component;
-namespace rsc = atta::resource;
-
 void CubeScript::update(cmp::Entity entity, float dt) {
     // The static variables are shared among all CubeScripts
     static const cmp::Entity prototype(1);
     static unsigned numClones = 0;
     static float time = 0;
 
-    int cloneId = entity.getCloneId();
+    cmp::Factory* factory = cmp::getFactory(prototype);
+
+    int cloneId = entity.getId() - factory->getFirstClone().getId();
 
     //----- Get number of clones -----//
     if (numClones == 0)
-        numClones = cmp::getFactory(prototype)->getNumClones();
+        numClones = factory->getMaxClones();
 
     //----- Advance time -----//
     // Use clone 0 to advance the time
     if (cloneId == 0) {
-        time += dt * 0.1;
+        time += dt;
         if (time > 2 * M_PI)
             time -= 2 * M_PI;
     }
 
     //----- Transform Component -----//
-    unsigned row = cloneId % (unsigned)sqrt(numClones);
-    unsigned col = cloneId / (unsigned)sqrt(numClones);
+    unsigned row = cloneId % (unsigned)std::sqrt(numClones);
+    unsigned col = cloneId / (unsigned)std::sqrt(numClones);
     float offset = 8 * M_PI / numClones * (col + row) + time;
 
     // Update clone transform
     cmp::Transform* t = entity.get<cmp::Transform>();
     t->position.x = row;
     t->position.y = col;
-    t->position.z = sin(offset);
+    t->position.z = 1.0f + std::sin(offset);
 
     //----- Material Component -----//
     // Set material resource
     std::string materialName = "Material " + std::to_string(cloneId);
-    rsc::Material* m = rsc::get<rsc::Material>(materialName);
+    res::Material* m = res::get<res::Material>(materialName);
     entity.get<cmp::Material>()->set(m);
 
     // Update material color
-    m->color.x = (t->position.z + 1.0f) * 0.5f;
-    m->color.y = 1.0f - m->color.x;
-    m->color.z = 1.0f;
+    atta::vec3f color;
+    color.x = t->position.z * 0.5f;
+    color.y = 1.0f - m->getColor().x;
+    color.z = 1.0f;
+    m->setColor(color);
 
     //----- Name Component -----//
-    cmp::Name* n = entity.get<cmp::Name>();
-    strcpy(n->name, ("Cube clone " + std::to_string(cloneId)).c_str());
+    entity.get<cmp::Name>()->set("Cube clone " + std::to_string(cloneId));
 }
